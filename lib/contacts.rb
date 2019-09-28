f43ae937627d1baec54f0da73b39859ffabc5d67
sarah <s.j.condon01@gmail.com>
2019-09-28 15:56:33 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -64,26 +64,3 @@ puts "//////"
 puts contacts ["Jon Snow"].keys
 puts contacts ["Jon Snow"].values
 
-
-
-
-
-# require 'pry'
-
-#   # This is the array we will be passing into the remove_strawberry method
-#   contacts = {
-#     "Jon Snow" => {
-#       name: "Jon",
-#       email: "jon_snow@thewall.we", 
-#       favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-#     },
-#     "Freddy Mercury" => {
-#       name: "Freddy",
-#       email: "freddy@mercury.com",
-#       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-#     }
-#   }
-  
-# def remove_strawberry(contacts)
-
-# end
